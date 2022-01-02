417979b656b1fe28e7c6b9bb284a25d5b461da3c
Maqsud <maqsudtolipov9@gmail.com>
2022-01-02 23:05:23 +0500

    Update findNemo1.js

--- findNemo1.js
+++ findNemo1.js
@@ -1,17 +1,22 @@
-const nemo = ['nemo'];
-const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-const large = new Array(1000).fill('nemo');
+// const nemo = ['nemo'];
+// const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+// const large = new Array(1000).fill('nemo');
 
-function findNemo(array) {
-  let t0 = performance.now();
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
-      console.log('Found NEMO! 😹');
-    }
-  }
-  let t1 = performance.now();
+// function findNemo(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemo') {
+//       console.log('Found NEMO! 😹');
+//     }
+//   }
+// }
 
-  console.log(`Call to find Nemo took: ${(t1 - t0)} milliseconds.`);
+// findNemo(large); // Big O: O(n) --> Linear Time
+
+const boxes = [0, 1, 2, 3, 4, 5];
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]); // O(1)
+  console.log(boxes[1]); // O(2)
 }
 
-findNemo(large);
+logFirstTwoBoxes(boxes); // O(2)

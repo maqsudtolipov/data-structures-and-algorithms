1f5ca0f86c5e1938e2751ced2c3bc1971261fbd7
Maqsud <maqsudtolipov9@gmail.com>
2022-01-02 22:44:39 +0500

    Update findNemo1.js

--- findNemo1.js
+++ findNemo1.js
@@ -1,11 +1,17 @@
 const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(1000).fill('nemo');
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
-      console.log('Found NEMO! 🐟');
+      console.log('Found NEMO! 😹');
     }
   }
+  let t1 = performance.now();
+
+  console.log(`Call to find Nemo took: ${(t1 - t0)} milliseconds.`);
 }
 
-findNemo(nemo);
+findNemo(large);

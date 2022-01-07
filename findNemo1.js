7b2ac4cc87e6e8d70c7934a72be39f329d1944f2
Maqsud <maqsudtolipov9@gmail.com>
2022-01-07 14:35:43 +0500

    Update findNemo1.js

--- findNemo1.js
+++ findNemo1.js
@@ -1,22 +1,35 @@
-// const nemo = ['nemo'];
-// const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-// const large = new Array(1000).fill('nemo');
+/*
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'hank', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'nemo'];
+const large = new Array(1000).fill('nemo');
 
-// function findNemo(array) {
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === 'nemo') {
-//       console.log('Found NEMO! 😹');
-//     }
-//   }
-// }
-
-// findNemo(large); // Big O: O(n) --> Linear Time
-
-const boxes = [0, 1, 2, 3, 4, 5];
-
-function logFirstTwoBoxes(boxes) {
-  console.log(boxes[0]); // O(1)
-  console.log(boxes[1]); // O(2)
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log('running')
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO! 😹');
+      break;
+    }
+  }
 }
 
-logFirstTwoBoxes(boxes); // O(2)
+findNemo(everyone); // O(n)
+*/
+
+function printFirstItemThenFirstHalfThenSayHi100Times(items) {
+  console.log(items[0]);
+
+  var middleIndex = Math.floor(items.length / 2);
+  var index = 0;
+
+  while (index < middleIndex) {
+    console.log(items[index]);
+    index++;
+  }
+
+  for (var i = 0; i < 100; i++) {
+    console.log('hi');
+  }
+}
+
+// O(n)

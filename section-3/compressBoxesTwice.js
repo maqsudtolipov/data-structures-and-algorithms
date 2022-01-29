40b11107fc8d130ecf1e573e980864d71f2c8521
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-01-29 15:57:31 +0500

    Learns Rules #3

---
+++ section-3/compressBoxesTwice.js
@@ -0,0 +1,11 @@
+function compressBoxesTwice(boxes, boxes2) {
+  boxes.forEach(function (boxrd) {
+    console.log(boxes);
+  });
+
+  boxes2.forEach(function (boxrd) {
+    console.log(boxes);
+  });
+}
+
+// O(n + m)
--- section-3/findNemo2.js
+++ section-3/findNemo2.js
@@ -1,22 +1,34 @@
-//#1 -- For loop in Javascript.
-// const nemo = ['nemo'];
-// const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-// const large = new Array(1000).fill('nemo')
+// #1 -- For loop in Javascript.
+const nemo = ['nemo'];
+const everyone = [
+  'dory',
+  'bruce',
+  'marlin',
+  'gill',
+  'bloat',
+  'nigel',
+  'squirt',
+  'darla',
+  'hank',
+  'nemo',
+];
+const large = new Array(1000).fill('nemo');
 
-// function findNemo(array) {
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === 'nemo') {
-//       console.log('Found NEMO 🐠!');
-//     }
-//   }
-// }
-
-// findNemo(large); // O(n) --> Linear Time
-
-const boxes = [0, 1, 2, 3, 4, 5];
-function logFirstTwoBoxes(boxes) {
-  console.log(boxes[0]); // O(1)
-  console.log(boxes[1]); // O(1)
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log('running');
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO 🐠!');
+    }
+  }
 }
 
-logFirstTwoBoxes(boxes) // O(1)
+findNemo(large); // O(n) --> Linear Time
+
+// const boxes = [0, 1, 2, 3, 4, 5];
+// function logFirstTwoBoxes(boxes) {
+//   console.log(boxes[0]); // O(1)
+//   console.log(boxes[1]); // O(1)
+// }
+
+// logFirstTwoBoxes(boxes) // O(1)

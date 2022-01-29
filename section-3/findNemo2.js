0d2b4e2d99f74b70714437081b7824ee8b38c494
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-01-29 15:13:32 +0500

    Coding challenge #1

--- section-3/findNemo2.js
+++ section-3/findNemo2.js
@@ -1,14 +1,22 @@
 //#1 -- For loop in Javascript.
-const nemo = ['nemo'];
-const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-const large = new Array(1000).fill('nemo')
+// const nemo = ['nemo'];
+// const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+// const large = new Array(1000).fill('nemo')
 
-function findNemo(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
-      console.log('Found NEMO 🐠!');
-    }
-  }
+// function findNemo(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemo') {
+//       console.log('Found NEMO 🐠!');
+//     }
+//   }
+// }
+
+// findNemo(large); // O(n) --> Linear Time
+
+const boxes = [0, 1, 2, 3, 4, 5];
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]); // O(1)
+  console.log(boxes[1]); // O(1)
 }
 
-findNemo(large); // O(n) --> Linear Time
+logFirstTwoBoxes(boxes) // O(1)

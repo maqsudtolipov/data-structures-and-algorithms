76448a3b44898bfe75d0f8a727650d20612a6d87
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-01-30 20:31:50 +0500

    Create spaceComplexity.js

---
+++ section-3/spaceComplexity.js
@@ -0,0 +1,23 @@
+/*
+function booooo(n) {
+  for (let i = 0; i < n.length; i++) {
+    console.log('booooooo!');
+  }
+}
+
+booooo([1, 2, 3, 4, 5]); // O(1)
+
+function arrayOfHiNTimes(n) {
+  let hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = 'hi';
+  }
+  return hiArray;
+}
+
+console.log(arrayOfHiNTimes(6)); // O(n)
+
+// Find first, Find Nth...
+console.log('sdkfjksadjfksadjfksajdfs'.length); // O(1)
+*/
+
